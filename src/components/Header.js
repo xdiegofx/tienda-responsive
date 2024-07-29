47a58a3caf5d1ffd24763b84e2cf3fcf54c3b594
crojasp1 <camilorojasprieto@gmail.com>
2024-07-29 12:53:09 -0500

    Componente Navbar rediseñado junto con sus estilos, carpeta Styles creada junto con Assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,42 @@
 import React from "react";
+import "../styles/Header.css";
+import lupa from "../assets/lupa.png";
+import carrito from "../assets/carrito_compras.png";
 
 const Header = () => {
-    return (
-        <header>
-            <div className="logo">Mi tienda</div>
-            <nav>
-                <li><a href="#home">Inicio</a></li>
-                <li><a href="#products">Productos</a></li>
-                <li><a href="#contact">Contacto</a></li> 
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <div className="header-container">
+        <nav>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#products">About</a>
+          </li>
+          <li>
+            <a href="#contact">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </nav>
+        <div className="header-icons">
+          <span>
+        
+            <img src={lupa} alt="Imagen lupa"></img>
+          </span>
+          <span>
+        
+            <img src={carrito} alt="Imagen carrito"></img>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
