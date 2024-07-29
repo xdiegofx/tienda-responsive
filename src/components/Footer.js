aa66be76addd2a84d108ae0dd8aec0496cb31397
Diego Ruiz <diegof.0729@gmail.com>
2024-07-29 08:00:35 -0500

    componentes footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Contacto: info@mitienda.com</p>
+      <p>&copy; 2023 MiTienda. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
