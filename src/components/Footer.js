0be623f627e2ef32445943a66abd9075d2d4cb37
Jarey22 <juansrm02@gmail.com>
2024-07-30 15:00:57 -0500

    Diseño del footer #1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import "../styles/Footer.css";
+import whatsapp from "../assets/whatsapp.png";
+import insta from "../assets/instagram.png";
+import facebook from "../assets/facebook.png";
+import x from "../assets/x.png";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Contacto: info@mitienda.com</p>
-      <p>&copy; 2023 MiTienda. Todos los derechos reservados.</p>
+    <footer className='footer-final'>
+      <p><strong>Contacto </strong>: info@mitienda.com</p>
+      <img className="iconos" src={whatsapp} alt="Imagen whatsapp"></img>
+      <img className="iconos" src={insta} alt="Imagen carrito"></img>
+      <img className="iconos" src={facebook} alt="Imagen carrito"></img>
+      <img className="iconos" src={x} alt="Imagen carrito"></img>
+      <p><strong>&copy; 2023 MiTienda.</strong> <br/>  Todos los derechos reservados.</p>
     </footer>
   );
 };
+
 
 export default Footer;
