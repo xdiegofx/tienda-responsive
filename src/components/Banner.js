3817d276207413474aea47acd5c323411e002047
crojasp1 <camilorojasprieto@gmail.com>
2024-07-30 02:51:37 -0500

    Estilando Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "../styles/Banner.css";
+
 
 const Banner = () => {
     return (
         <div className="banner">
+
             <h1>Bienvenidos Al Remante</h1>
             <p>Los mejores productos a los mejores precios con los mejores descuentos</p>
         </div>
