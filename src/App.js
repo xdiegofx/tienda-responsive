e1a23a3ac43f6d603f7b4336d2993f67d1708b25
Jarey22 <juansrm02@gmail.com>
2024-07-31 06:16:18 -0500

    detalles finales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Banner from './components/Banner';
-import Productos from './components/Productos';
 import Slider from './components/Slider';
 import Footer from './components/Footer';
 import Marcas from './components/Marcas';
-
+/*
 const productos = [
   { id: 1, nombre: 'Producto 1', precio: '$10', imagen: 'ruta/a/imagen1.jpg' },
   { id: 2, nombre: 'Producto 2', precio: '$20', imagen: 'ruta/a/imagen2.jpg' },
   { id: 3, nombre: 'Producto 3', precio: '$30', imagen: 'ruta/a/imagen3.jpg' }
 ];
-
+*/
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
       <Marcas/>
-      <Productos productos={productos} />
       <Slider />
       <Footer />
     </div>
